df2f9175fb4c3987ce99cd9f36c61fb0497a37a6
YayaProject <117073849+YayaProject@users.noreply.github.com>
2022-11-05 21:20:27 +0800

    Menambahkan script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+            $(".menu-toggle").click(function () {
+                $('nav').toggleClass('active');
+            })
+        })
+
+$(document).ready(function () {
+            $(".non-active").click(function () {
+                $('nav').toggleClass('active');
+            })
+        })
