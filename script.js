9b4bc1e119eb5f778d4b0380ae4f150c75d2ac2d
Yaya Project <117073849+YayaProject@users.noreply.github.com>
2022-11-22 13:23:48 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-document.getElementByClass("tombolview").onclick = function() {
-     Swal.fire(
-  'Coming Soon',
-  'Sabar Developer Mau Ujian',
-  'info'
-)
-  }
+
 $(document).ready(function () {
             $(".menu-toggle").click(function () {
                 $('nav').toggleClass('active');
