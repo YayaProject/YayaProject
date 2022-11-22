347d0323783cb449399ef25f2e1d16daaa2601f1
Yaya Project <117073849+YayaProject@users.noreply.github.com>
2022-11-22 15:29:29 +0800

    update script.js

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ $(document).ready(function () {
                 $('nav').toggleClass('active');
             })
         })
+document.getElementById("tombolview").onclick = function() {
+     Swal.fire(
+  'Coming Soon',
+  'Sabar Developer Mau Ujian',
+  'info'
+)
+  }
+document.getElementById("tombol-about1").onclick = function() {
+     Swal.fire(
+  'Jadwal Mata Pelajaran',
+  'Project ini berasal dari jadwal mata pelajaran kelas X Mipa 4'
+)
+  }
+  document.getElementById("tombol-about2").onclick = function() {
+     Swal.fire(
+  'Menu Login',
+  'Project ini adalah form login untuk masuk ke situs ini'
+)
+  }
+  document.getElementById("tombol-about3").onclick = function() {
+     Swal.fire(
+  '',
+  'Project ini adalah form login untuk masuk ke situs ini'
+)
+  }
