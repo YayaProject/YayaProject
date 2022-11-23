16f00d3ab11118a0de44c17c7cd6225bb1579301
Yaya Project <117073849+YayaProject@users.noreply.github.com>
2022-11-23 08:41:53 +0800

    update script.js

--- script.js
+++ script.js
@@ -67,6 +67,6 @@ document.getElementById("tombol-about8").onclick = function() {
 document.getElementById("tombol-about9").onclick = function() {
      Swal.fire(
   'Coming Soon',
-  '- Update project 1',                       '- Update UI/UX', '- Update responsif mode landscape'
+  'Update project 1, UI/UX, dan responsif mode landscape'
 )
   }
