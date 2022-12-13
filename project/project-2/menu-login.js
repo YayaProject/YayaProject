8346f98c1ccd89384af7a9f6df0bc1d6b2e066ee
Yaya Project <117073849+YayaProject@users.noreply.github.com>
2022-12-13 11:00:29 +0800

    UPDATE SWEET ALERT

--- project/project-2/menu-login.js
+++ project/project-2/menu-login.js
@@ -7,3 +7,13 @@ document.getElementById("kolom2").onclick = function () {
   timer: 1500
 })
       };
+      
+document.getElementById("kolom3").onclick = function () {
+        Swal.fire({
+  position: 'top-end',
+  icon: 'success',
+  title: 'Password Berhasil Diubah',
+  showConfirmButton: false,
+  timer: 1500
+})
+      };
