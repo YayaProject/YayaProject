99d61ceb73d1fc93c503c50eaac980a99ab5b7c5
Yaya Project <117073849+YayaProject@users.noreply.github.com>
2022-12-13 10:53:40 +0800

    UPDATE SWEET ALERT

--- project/project-2/menu-login.js
+++ project/project-2/menu-login.js
@@ -1,1 +1,9 @@
-
+document.getElementById("kolom2").onclick = function () {
+        Swal.fire({
+  position: 'top-end',
+  icon: 'success',
+  title: 'Login Berhasil',
+  showConfirmButton: false,
+  timer: 1500
+})
+      };
